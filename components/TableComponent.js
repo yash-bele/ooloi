3dd3d7d0d3aa52612cd9a2f96cc49d34a690b95e
yash-bele <yashgbele@gmail.com>
2022-11-24 10:49:40 +0530

    commit

---
+++ components/TableComponent.js
@@ -0,0 +1,144 @@
+import { useState } from 'react';
+import { BiSortAlt2 } from 'react-icons/bi';
+const th = [
+  ['name', 'Name'],
+  ['city', 'City'],
+  ['email', 'Email Address'],
+  ['date', 'Joining Date'],
+  ['role', 'Role'],
+];
+
+const TableComponent = ({ data, tableConfig }) => {
+  const [sort, setSort] = useState(data);
+  const [ascending, setAscending] = useState({
+    name: 0,
+    city: 0,
+    email: 0,
+    date: 0,
+    role: 0,
+  });
+
+  const thFilter = th.filter((i) => tableConfig.some((j) => j.includes(i[0])));
+  const x = tableConfig.filter((i) => i.includes('WithSort'));
+
+  const handleClick = (action) => {
+    if (action === 'name' && !ascending.name) {
+      setSort(
+        [...data].sort((a, b) => (a.person.name < b.person.name ? -1 : 1))
+      );
+      return setAscending({ name: 1, city: 0, email: 0, date: 0, role: 0 });
+    }
+    if (action === 'name' && ascending.name) {
+      setSort(
+        [...data].sort((a, b) => (a.person.name > b.person.name ? -1 : 1))
+      );
+      return setAscending({ name: 0, city: 0, email: 0, date: 0, role: 0 });
+    }
+    if (action === 'city' && !ascending.city) {
+      setSort([...data].sort((a, b) => (a.city < b.city ? -1 : 1)));
+      return setAscending({ name: 0, city: 1, email: 0, date: 0, role: 0 });
+    }
+    if (action === 'city' && ascending.city) {
+      setSort([...data].sort((a, b) => (a.city > b.city ? -1 : 1)));
+      return setAscending({ name: 0, city: 0, email: 0, date: 0, role: 0 });
+    }
+    if (action === 'email' && !ascending.email) {
+      setSort([...data].sort((a, b) => (a.email < b.email ? -1 : 1)));
+      return setAscending({ name: 0, city: 0, email: 1, date: 0, role: 0 });
+    }
+    if (action === 'email' && ascending.email) {
+      setSort([...data].sort((a, b) => (a.email > b.email ? -1 : 1)));
+      return setAscending({ name: 0, city: 0, email: 0, date: 0, role: 0 });
+    }
+    if (action === 'date' && !ascending.date) {
+      setSort(
+        [...data].sort((a, b) => {
+          a = a.joiningDate.split('/').reverse().join();
+          b = b.joiningDate.split('/').reverse().join();
+          return a < b ? -1 : 1;
+        })
+      );
+      return setAscending({ name: 0, city: 0, email: 0, date: 1, role: 0 });
+    }
+    if (action === 'date' && ascending.date) {
+      setSort(
+        [...data].sort((a, b) => {
+          a = a.joiningDate.split('/').reverse().join();
+          b = b.joiningDate.split('/').reverse().join();
+          return a > b ? -1 : 1;
+        })
+      );
+      return setAscending({ name: 0, city: 0, email: 0, date: 0, role: 0 });
+    }
+    if (action === 'role' && !ascending.role) {
+      setSort([...data].sort((a, b) => (a.role < b.role ? -1 : 1)));
+      return setAscending({ name: 0, city: 0, email: 0, date: 0, role: 1 });
+    }
+    if (action === 'role' && ascending.role) {
+      setSort([...data].sort((a, b) => (a.role > b.role ? -1 : 1)));
+      return setAscending({ name: 0, city: 0, email: 0, date: 0, role: 0 });
+    }
+  };
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          {thFilter.map((i, j) => {
+            return (
+              <th
+                key={j}
+                className={`h-[40px] ${
+                  i[0] === 'email' ? 'w-[220px]' : 'w-[175px]'
+                }`}
+              >
+                {i[1]}{' '}
+                {x.some((k) => k.includes(i[0])) && (
+                  <BiSortAlt2
+                    className='inline cursor-pointer ml-1 -mt-0.5 text-lg'
+                    onClick={() => handleClick(i[0])}
+                  />
+                )}
+              </th>
+            );
+          })}
+        </tr>
+      </thead>
+
+      <tbody>
+        {sort.map((i, j) => {
+          const { person, city, email, joiningDate, role } = i;
+          return (
+            <tr
+              key={person.name}
+              className={`${j % 2 === 0 && 'bg-[#F5F5F5]'}`}
+            >
+              {thFilter.some((k) => k[0] === 'name') && (
+                <td>
+                  <img
+                    src={person.avatar}
+                    alt={person.name}
+                    className={`inline mr-3 w-[22.15px] ${
+                      j % 2 === 0 && 'bg-[#F5F5F5]'
+                    }`}
+                  />
+                  {person.name}
+                </td>
+              )}
+              {thFilter.some((k) => k[0] === 'city') && <td>{city}</td>}
+              {thFilter.some((k) => k[0] === 'email') && (
+                <td>
+                  <a href='#'>{email}</a>
+                </td>
+              )}
+              {thFilter.some((k) => k[0] === 'date') && <td>{joiningDate}</td>}
+              {thFilter.some((k) => k[0] === 'role') && <td>{role}</td>}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableComponent;
---
+++ pages/index.js
@@ -0,0 +1,64 @@
+import Head from 'next/head';
+import { FaMobileAlt, FaDesktop, FaLongArrowAltRight } from 'react-icons/fa';
+import { FiAlertTriangle } from 'react-icons/fi';
+import TableComponent from '../components/TableComponent';
+import testTableData from '../testTableData.json';
+
+const Home = () => (
+  <>
+    <Head>
+      <title>Ooloi</title>
+      <meta name='description' content='Website' />
+      <link
+        rel='icon'
+        href='https://static.wixstatic.com/media/d379bd_b94591f1c31044119a66824f17465179%7Emv2.png/v1/fill/w_32%2Ch_32%2Clg_1%2Cusm_0.66_1.00_0.01/d379bd_b94591f1c31044119a66824f17465179%7Emv2.png'
+      />
+    </Head>
+    <main className='absolute w-full h-full hidden lg:flex justify-center'>
+      <section className='space-y-10'>
+        <div className='w-px h-px'></div>
+        <TableComponent
+          data={testTableData}
+          tableConfig={[
+            'nameWithSort',
+            'cityWithSort',
+            'emailWithSort',
+            'dateWithSort',
+            'roleWithSort',
+          ]}
+        />
+        <TableComponent
+          data={testTableData}
+          tableConfig={['nameWithSort', 'email', 'role']}
+        />
+        <TableComponent
+          data={testTableData}
+          tableConfig={['email', 'dateWithSort', 'roleWithSort']}
+        />
+        <TableComponent
+          data={testTableData}
+          tableConfig={['name', 'cityWithSort', 'date', 'roleWithSort']}
+        />
+        <div className='w-px h-px'></div>
+      </section>
+    </main>
+    <main className='absolute w-full h-full flex lg:hidden justify-center items-center'>
+      <section className='-mt-10 shadow-md p-5 rounded-md'>
+        <FiAlertTriangle className='text-center text-5xl w-full mb-5 text-red-500' />
+        <h1 className='font-bold text-center text-base text-slate-700'>
+          You are on small screen.
+        </h1>
+        <h1 className='font-bold text-center text-base text-slate-700'>
+          Switch to desktop size screen...
+        </h1>
+        <div className='flex items-center justify-center space-x-10 mt-5'>
+          <FaMobileAlt className='text-5xl text-blue-500' />
+          <FaLongArrowAltRight className='text-5xl text-slate-700' />
+          <FaDesktop className='text-5xl text-blue-500' />
+        </div>
+      </section>
+    </main>
+  </>
+);
+
+export default Home;
